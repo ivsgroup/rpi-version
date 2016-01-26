0812f0a1d6079f40cc636b132f51427cb5286b2f
François Leurent <fleurent@ivsweb.com>
2016-01-26 17:11:20 +0100

    Initial revision

---
+++ README.md
@@ -0,0 +1,17 @@
+= Rpi version
+
+Detect current RPI revision (from CPU infos http://www.raspberrypi-spy.co.uk/2012/09/checking-your-raspberry-pi-board-version/)
+
+
+= API
+
+```
+var model = require('rpi-version')();
+//return current RPI model OR undefined
+
+```
+
+
+= Credits
+* Mejdi Ourir
+
---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+var CPU_INFO_FILE_PATH = "/proc/cpuinfo";
+
+var ras_tab = {
+  "0002"   : "Model_B_Revision_1.0",
+  "0003"   : "Model_B_Revision_1.0_ECN0001",
+  "0004"   : "Model_B_Revision_2.0",
+  "0005"   : "Model_B_Revision_2.0",
+  "0006"   : "Model_B_Revision_2.0",
+  "0007"   : "Model_A",
+  "0008"   : "Model_A",
+  "0009"   : "Model_A",
+  "0010"   : "Model_B+",
+  "0011"   : "Compute_Module",
+  "0012"   : "Model_A+",
+  "000d"   : "Model_B_Revision_2.0",
+  "000e"   : "Model_B_Revision_2.0",
+  "000f"   : "Model_B_Revision_2.0",
+  "a01041" : "Model_B_PI_2",
+  "a21041" : "Model_B_PI_2",
+  "900092" : "Model_PiZero"
+};
+
+
+
+module.exports = function(){
+  var cpu_info = fs.readFileSync(CPU_INFO_FILE_PATH).toString();
+  cpu_info = cpu_info.slice(cpu_info.lastIndexOf("Revision") , cpu_info.length);
+  revision = cpu_info.slice(cpu_info.indexOf(":")+1 , cpu_info.indexOf("\n")).trim();
+  return ras_tab[revision]
+}
+ No newline at end of file

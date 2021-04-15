3bed4c2ca0d93e13073b7268f14f22ad39e9dc71
Roger Hardiman <opensource@rjh.org.uk>
2021-04-15 09:26:26 +0100

    If model is unknown, print the revision
    Would have been nicer to have added a GetRevision() to index.js but could not find a way to export a
    2nd function from index.js when the existing default function has no function name

--- test/show_version.js
+++ test/show_version.js
@@ -1,4 +1,14 @@
 var model = require('../index.js')();
 
-console.log("Model is " + model);
+if (model) {
+  console.log("Model is " + model);
+} else {
+  // for debugging, get the hardware versions so we can add ths into index.js
+  var fs = require('fs');
+  var CPU_INFO_FILE_PATH = "/proc/cpuinfo";
+  var cpu_info = fs.readFileSync(CPU_INFO_FILE_PATH).toString();
+  cpu_info = cpu_info.slice(cpu_info.lastIndexOf("Revision") , cpu_info.length);
+  revision = cpu_info.slice(cpu_info.indexOf(":")+1 , cpu_info.indexOf("\n")).trim();
+  console.log("Unknown revision " + revision);
+}
 
